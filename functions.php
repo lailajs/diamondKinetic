1ddd445242c2ba7fefd245bb2f98c5937223993c
lailajs <55669130+lailajs@users.noreply.github.com>
2021-04-22 15:28:53 -0400

    Initial commit

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+  require get_template_directory() . '/inc/example-post-type.php';
+  require get_template_directory() . '/inc/enqueue-scripts.php';
+  require get_template_directory() . '/inc/home-page-settings.php';
+  require get_template_directory() . '/inc/example-page-settings.php';
+  require get_template_directory() . '/inc/customizer.php';
+  require get_template_directory() . '/inc/template_functions.php';
+?>
+
